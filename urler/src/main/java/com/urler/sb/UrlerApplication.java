315f2c694cd4e1d92e4d5b95ea27a95b742417e5
Byeori Kim <bk.byeori.kim@gmail.com>
2025-01-26 19:04:29 -0800

    initializing

---
+++ urler/src/main/java/com/urler/sb/UrlerApplication.java
@@ -0,0 +1,13 @@
+package com.urler.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlerApplication.class, args);
+	}
+
+}
---
+++ urler/src/test/java/com/urler/sb/UrlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urler.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
