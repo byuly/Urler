fe6442cc4825bfff2fc7107729a63df1e6aac962
Byeori KIm <bk.byeori.kim@gmail.com>
2025-01-27 23:50:50 -0800

    user table schema defined

--- backend-sb/src/main/java/com/urler/sb/table/User.java
+++ backend-sb/src/main/java/com/urler/sb/table/User.java
@@ -1,4 +1,17 @@
 package com.urler.sb.table;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
 public class User {
+    @Id // Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // ID will be generated in incremental order
+    private Long id;
+    private String email;
+    private String username;
+    private String password;
+    private String role = "ROLE_USER";
 }
