641e31e47631137819d49d731e23171e9d8a2c74
Byeori KIm <bk.byeori.kim@gmail.com>
2025-03-31 23:40:29 -0700

    refactor (testing out stuff)

--- backend-sb/src/main/java/com/urler/service/UserDetailsImpl.java
+++ backend-sb/src/main/java/com/urler/service/UserDetailsImpl.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.io.Serial;
 import java.util.Collection;
 import java.util.Collections;
