29dfa58f9474744ab9a38e498863f31911b23476
Byeori Kim <bk.byeori.kim@gmail.com>
2025-04-05 11:04:42 -0700

    JWT implementations

--- backend-sb/src/main/java/com/urler/security/jwt/JwtUtils.java
+++ backend-sb/src/main/java/com/urler/security/jwt/JwtUtils.java
@@ -1,8 +1,28 @@
 package com.urler.security.jwt;
 
+import com.urler.service.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.validation.annotation.Validated;
+
+import java.security.Key;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private String jwtExpiration;
+
     // pass in token with authorization header -> Bearer <TOKEN> is the format
     public String getJwtFromHeader(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
@@ -10,5 +30,24 @@ public class JwtUtils {
             return bearerToken.substring(7);
         }
         return null;
+    }
+
+    // generating token
+    public String generateJwtToken(UserDetailsImpl userDetails) {
+        String username = userDetails.getUsername();
+        String role = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority) // difference between this and .map(authority -> authority.getAuthority())?
+                .collect(Collectors.joining(","));
+        return Jwts.builder()
+                .subject(username)
+                .claim("roles", role)
+                .issuedAt(new Date())
+                .expiration(new Date((new Date().getTime()+ jwtExpiration)))
+                .signWith(key())
+                .compact();
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 }
