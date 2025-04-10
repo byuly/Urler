ea286f67caf32e221519a7a7ba38390f5f93246d
Byeori KIm <bk.byeori.kim@gmail.com>
2025-04-09 23:57:48 -0700

    jwt filter

--- backend-sb/src/main/java/com/urler/security/jwt/JwtAuthenticationResponse.java
+++ backend-sb/src/main/java/com/urler/security/jwt/JwtAuthenticationResponse.java
@@ -1,12 +1,12 @@
 package com.urler.security.jwt;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 // data class that represents authentication response
 @Data
+@AllArgsConstructor
 public class JwtAuthenticationResponse {
     private String token;
-
-
 }
