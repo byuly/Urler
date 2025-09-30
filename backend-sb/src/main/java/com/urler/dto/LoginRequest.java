1bac1e545f932ff93a1f0a1b4e6d369efb169b74
Byeori Kim <bk.byeori.kim@gmail.com>
2025-09-30 01:44:00 -0400

    login with spring security

---
+++ backend-sb/src/main/java/com/urler/dto/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.urler.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String username;
+    private String password;
+}
---
+++ backend-sb/src/main/java/com/urler/service/UserService.java
@@ -0,0 +1,46 @@
+package com.urler.service;
+
+import com.urler.dto.LoginRequest;
+import com.urler.repository.UserRepository;
+import com.urler.security.jwt.JwtAuthenticationResponse;
+import com.urler.security.jwt.JwtUtils;
+import com.urler.table.User;
+import com.urler.table.UserDetailsImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private PasswordEncoder passwordEncoder;
+    private UserRepository userRepository;
+    private AuthenticationManager authenticationManager;
+    private JwtUtils jwtUtils;
+
+    public User registerUser(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public JwtAuthenticationResponse authenticateUser(LoginRequest loginRequest){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
+                        loginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        String jwt = jwtUtils.generateJwtToken(userDetails);
+        return new JwtAuthenticationResponse(jwt);
+    }
+
+    public User findByUsername(String name) {
+        return userRepository.findByUsername(name).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with username: " + name)
+        );
+    }
+}
